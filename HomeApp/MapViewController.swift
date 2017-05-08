5bf467a13040452147b6bd56aa471b9d3909354f
jalejos <jalejos@nearsoft.com>
2017-05-08 16:44:21 -0600

    Add temporal sign out button

--- HomeApp/MapViewController.swift
+++ HomeApp/MapViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class MapViewController: UIViewController {
 
@@ -15,21 +16,10 @@ class MapViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func signOutTap(_ sender: Any) {
+        try! FIRAuth.auth()!.signOut()
+        dismiss(animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
