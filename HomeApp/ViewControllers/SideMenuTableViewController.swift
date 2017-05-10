f3e8f8563d373fe211c6203726d3cea9f1659059
jalejos <jalejos@nearsoft.com>
2017-05-10 16:23:38 -0600

    Add basic UI appearance

---
+++ HomeApp/ViewControllers/SideMenuTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SideMenuTableViewController.swift
+//  HomeApp
+//
+//  Created by Alejos on 5/10/17.
+//  Copyright © 2017 Alejos. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
