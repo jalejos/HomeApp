e4eb84d99f137cdba615f1534ad42ab11ae50131
jalejos <jalejos@nearsoft.com>
2017-05-25 17:47:29 -0600

    Include dialog options on alerts

--- HomeApp/Utilities/AlertViewUtility.swift
+++ HomeApp/Utilities/AlertViewUtility.swift
@@ -14,4 +14,11 @@ struct AlertViewUtility {
         alert.addAction(UIAlertAction(title: button, style: .default, handler: nil))
         controller.present(alert, animated: true, completion: nil)
     }
+    
+    static func showAlertDialog(title: String?, message: String?, firstOption: String?, firstHandler: @escaping (UIAlertAction) -> (), secondOption: String?, secondHandler: @escaping (UIAlertAction) -> (), controller: UIViewController) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: firstOption, style: .default, handler: firstHandler))
+        alert.addAction(UIAlertAction(title: secondOption, style: .default, handler: secondHandler))
+        controller.present(alert, animated: true, completion: nil)
+    }
 }
