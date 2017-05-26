3e915ddb05f13e97e2a0e9076e78b9c81466193c
jalejos <jalejos@nearsoft.com>
2017-05-26 14:38:25 -0600

    Add user specific data storage

--- HomeApp/HouseRepository.swift
+++ HomeApp/HouseRepository.swift
@@ -8,16 +8,19 @@
 
 import Foundation
 import UIKit
+import FirebaseAuth
 import FirebaseDatabase
 import FirebaseStorage
 
 struct HouseRepository {
-    
     static let houseRef = FIRDatabase.database().reference(withPath: "houses")
-    static let imageRef = FIRStorage.storage().reference()
+    static let imageRef = FIRStorage.storage().reference(withPath: "houses")
     
     static func addHouse(dict: [String: Any], image: UIImage, errorHandler: @escaping (Error?) -> ()) {
-        houseRef.child(dict["address"] as! String).setValue(dict)
-        imageRef.child(dict["address"] as! String).put(UIImagePNGRepresentation(image)!)
+        guard let userID = FIRAuth.auth()?.currentUser?.uid else { return }
+        let userHouseRef = houseRef.child(userID)
+        let userImageRef = imageRef.child(userID)
+        userHouseRef.child(dict["address"] as! String).setValue(dict)
+        userImageRef.child(dict["address"] as! String).put(UIImagePNGRepresentation(image)!)
     }
 }
