261275eedff2b448e689748d57739798e9b0ff42
jalejos <jalejos@nearsoft.com>
2017-05-30 09:55:51 -0600

    Add tableview to your homes

--- HomeApp/ViewControllers/YourHomesViewController.swift
+++ HomeApp/ViewControllers/YourHomesViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class YourHomesViewController: UIViewController {
 
     @IBOutlet weak var addButton: UIBarButtonItem!
+    @IBOutlet weak var tableView: UITableView!
+    
+    fileprivate var houseArray: [Any] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,4 +25,18 @@ class YourHomesViewController: UIViewController {
         navigationItem.title = "YOUR-HOME".localized()
         addButton.title = "ADD-HOME".localized()
     }
+}
+
+extension YourHomesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return houseArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension YourHomesViewController: UITableViewDelegate {
+    
 }
